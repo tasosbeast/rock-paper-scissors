1b52fad4c0a41038ec7d9aaea728af6dfb054828
tasosbeast <antantos@iekdelta.gr>
2023-11-12 19:29:37 +0200

    Add meaningful comments

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-//create a variable computerchoice
-//create a variable for the players selection 
+
+// This code stores the player and computer's choice, as well as the scores of each player.
 
 let playerChoice ;
 let computerChoice ;
 let myScore = 0;
 let computerScore = 0;
-// make a function that will generate a random choice between rock paper and scissors
+
+
+// Function to generate computer's choice
+
 function getComputerChoice() {
+    // Generate a random number between 0 and 2
     let randomNumber = Math.floor(Math.random() * 3);
+     // Map the random number to a choice of 'Rock', 'Paper', or 'Scissors'
     if (randomNumber === 0) {
         return 'Rock'
     } else if(randomNumber === 1){
@@ -19,9 +24,16 @@ function getComputerChoice() {
 
 // create a function for the players choice to eliminate bugs and wrong words
 function getPlayerChoice(){
+    // Prompt the user to make a choice
     let playersInput =  prompt('Rock, Paper, Scissors');
+    
+    // Convert the choice to lowercase
     playersInput = playersInput.toLowerCase();
+    
+    // Capitalize the first letter of the choice
     playersInputCapitalize = playersInput.replace(playersInput[0], playersInput[0].toUpperCase())
+    
+    // Validate the choice and return it
     if(playersInputCapitalize === 'Rock'){
         return 'Rock';
     } else if (playersInputCapitalize === 'Paper'){
@@ -35,7 +47,6 @@ function getPlayerChoice(){
 
 
 //create a function that plays a round and return the outcome
-
 function playRound(playerChoice, computerChoice){
     if (playerChoice === computerChoice) {
         return "It's a tie!"
@@ -60,27 +71,39 @@ function playRound(playerChoice, computerChoice){
     }
 }
 
-function game(){
-    for (let i = 1; i <= 5; i++){
-        playerChoice = getPlayerChoice()
-        computerChoice = getComputerChoice()
+
+// play for 5 rounds and show the winner based on higher score
+function game() {
+    // Welcome messages
+    console.log("Welcome to the game!")
+    console.log("You are going to play 5 rounds of Rock, Paper, Scissors")
+    console.log("The winner is the one that has more points at the end of the 5 rounds")
+
+
+    // Loop 5 times for 5 rounds of the game
+    for (let i = 1; i <= 5; i++) {
+        // Get the player's choice
+        let playerChoice = getPlayerChoice();
+        
+        // Get the computer's choice
+        let computerChoice = getComputerChoice();
+        
+        // Log the result of the round
         console.log(playRound(playerChoice, computerChoice));
     }
 
+    // Determine and log the overall winner
     if (myScore > computerScore) {
-        console.log('You win the game!!!')
+        console.log('You win the game!!!');
     } else if (computerScore > myScore) {
-        console.log('You lost the game!!')
-    } else{
-        console.log("the game is a TIE!!!")
+        console.log('You lost the game!!');
+    } else {
+        console.log("The game is a TIE!!!");
     }
-
-    
 }
 
-
-game()
-
+// Start the game
+game();
 
 
 
