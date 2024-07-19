ccd218f974823e2dc8c04c87c4ac7b2f4facdf54
tasosbeast <antantos@iekdelta.gr>
2024-07-20 00:04:43 +0300

    chore: Refactor game logic,

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber === 1) {
@@ -10,7 +5,7 @@ function getComputerChoice() {
   } else if (randomNumber === 2) {
     return "Paper";
   } else {
-    return "scissors";
+    return "Scissors";
   }
 }
 
@@ -19,31 +14,47 @@ function getHumanChoice() {
   return humanPrompt[0].toUpperCase() + humanPrompt.slice(1).toLowerCase();
 }
 
-function playRound(human, computer) {
-  console.log(human, computer);
-  if (
-    (human === "Rock" && computer === "Rock") ||
-    (human === "Paper" && computer === "Paper") ||
-    (human === "Scissors" && computer === "Scissors")
-  ) {
-    console.log("Game is Draw");
-  } else if (
-    (human === "Rock" && computer === "Scissors") ||
-    (human === "Paper" && computer === "Rock") ||
-    (human === "Scissors" && computer === "Paper")
-  ) {
-    humanScore++;
-    console.log(`You win ${human} beats ${computer}`);
-  } else if (
-    (computer === "Rock" && human === "Scissors") ||
-    (computer === "Paper" && human === "Rock") ||
-    (computer === "Scissors" && human === "Paper")
-  ) {
-    computerScore++;
-    console.log(`You lose ${computer} wins ${human}`);
-  } else {
-    console.log("Wrong Choice");
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(human, computer) {
+    console.log(human, computer);
+    if (
+      (human === "Rock" && computer === "Rock") ||
+      (human === "Paper" && computer === "Paper") ||
+      (human === "Scissors" && computer === "Scissors")
+    ) {
+      console.log("Game is Draw");
+    } else if (
+      (human === "Rock" && computer === "Scissors") ||
+      (human === "Paper" && computer === "Rock") ||
+      (human === "Scissors" && computer === "Paper")
+    ) {
+      humanScore++;
+      console.log(`You win ${human} beats ${computer}`);
+    } else if (
+      (computer === "Rock" && human === "Scissors") ||
+      (computer === "Paper" && human === "Rock") ||
+      (computer === "Scissors" && human === "Paper")
+    ) {
+      computerScore++;
+      console.log(`You lose ${computer} wins ${human}`);
+    } else {
+      console.log("Wrong Choice");
+    }
+  }
+  let humanSelection;
+  let computerSelection;
+  for (let i = 0; i < 5; i++) {
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+  }
+  if (humanScore > computerScore) {
+    console.log("Human win the Game");
+  } else if (computerScore > humanScore) {
+    console.log("Computer win the Game");
   }
 }
 
-playRound(humanSelection, computerSelection);
+playGame();
