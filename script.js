694c74a4775617d0e12fd53f52f5427977840e8d
tasosbeast <antantos@iekdelta.gr>
2024-10-08 22:26:07 +0300

    Refactor game result display and add winner display

--- script.js
+++ script.js
@@ -35,6 +35,13 @@ const gameDiv = document.querySelector(".game");
 const resultsDiv = document.querySelector(".results");
 const resultDivs = document.querySelectorAll(".results__result");
 
+const resultWinner = document.querySelector(".results__winner");
+const resultText = document.querySelector(".results__text");
+
+const playAgainBtn = document.querySelector(".play-again");
+
+const scoreNumber = document.querySelector(".score__number");
+let score = 0;
 // game logic
 
 choiceButtons.forEach((button) => {
@@ -48,6 +55,7 @@ choiceButtons.forEach((button) => {
 function choose(choice) {
   const aichoice = aiChoose();
   displayResults([choice, aichoice]);
+  displayWinner([choice, aichoice]);
 }
 
 function aiChoose() {
@@ -69,3 +77,48 @@ function displayResults(results) {
   gameDiv.classList.toggle("hidden");
   resultsDiv.classList.toggle("hidden");
 }
+
+function displayWinner(results) {
+  setTimeout(() => {
+    const userWins = isWinner(results);
+    const aiWins = isWinner(results.reverse());
+
+    if (userWins) {
+      resultText.innerHTML = `You win`;
+      resultDivs[0].classList.toggle("winner");
+      keepScore(1);
+    } else if (aiWins) {
+      resultText.innerHTML = `You lose`;
+      resultDivs[1].classList.toggle("winner");
+      keepScore(-1);
+    } else {
+      resultText.innerHTML = `It's a tie`;
+    }
+    resultWinner.classList.toggle("hidden");
+    resultsDiv.classList.toggle("show-winner");
+  }, 1000);
+}
+
+function isWinner(results) {
+  return results[0].beats === results[1].name;
+}
+
+// score
+function keepScore(point) {
+  score += point;
+  scoreNumber.innerText = score;
+}
+
+// play again
+playAgainBtn.addEventListener("click", () => {
+  gameDiv.classList.toggle("hidden");
+  resultsDiv.classList.toggle("hidden");
+  resultDivs.forEach((resultDiv) => {
+    resultDiv.innerHTML = "";
+    resultDiv.classList.remove("winner");
+  });
+
+  resultText.innerText = "";
+  resultWinner.classList.toggle("hidden");
+  resultsDiv.classList.toggle("show-winner");
+});
